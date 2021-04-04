2cd306110fa734d4642884cb0fa75d1e58d867f4
Alexander Chori <alexandrchori@gmail.com>
2021-04-04 17:14:55 +0300

    Set progress bar to indeterminate mode (no animation bug on MacOS L&F)

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,6 @@ dependencies {
     testImplementation(kotlin("test-junit5"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
-    implementation(kotlin("stdlib-jdk8"))
 }
 
 tasks.test {
